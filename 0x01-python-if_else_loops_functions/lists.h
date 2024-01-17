395297c4526ca991f9da5f5394cf1c8327f02d6a
softontechsolution <softontechsolution@gmail.com>
2024-01-17 05:47:29 -0800

    task 13 done

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -16,7 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-def islower(c);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
