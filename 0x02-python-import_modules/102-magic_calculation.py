426661eea6f0e0c441e513d42399763861c6a547
softontechsolution <softontechsolution@gmail.com>
2024-01-24 12:26:58 -0800

    task 8 done

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+
+    if a < b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+
+    return sub(a, b)
