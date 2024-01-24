0e3c9a551124bef5e7adc8cf5061d89942cf797d
softontechsolution <softontechsolution@gmail.com>
2024-01-24 12:40:28 -0800

    task 8 updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,10 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
-
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-
         for i in range(4, 6):
             c = add(c, i)
-
         return c
-
-    return sub(a, b)
+    else:
+        return sub(a, b)
