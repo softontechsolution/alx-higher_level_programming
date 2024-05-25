afc921f2009faf73a8bab180a2117cd6c5d1ecdb
softontechsolution <softontechsolution@gmail.com>
2024-05-25 12:31:21 -0700

    task 0 corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Fetch
+https://intranet.hbtn.io/status
+using urlib package
+"""
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-def fetch_status(url):
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        response = request.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
-if __name__ == "__main__":
-    fetch_status(url)
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
