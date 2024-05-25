72e675fb1e9b5ad7847771f440ddf52cce2f608d
softontechsolution <softontechsolution@gmail.com>
2024-05-25 12:52:05 -0700

    checker fixed

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""" Takes in a URL and sends a request to the URL
-and displays the body of the response (decoded in utf-8)
-"""
+"""Sends a request to a given URL and displays the response body.
 
-import urllib.request
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
+import urllib.error
+import urllib.request
+
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf-8"))
-    except urllib.error.HTTPError as error:
-        print("Error code: {}".format(error.code))
-    except:
-        pass
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
